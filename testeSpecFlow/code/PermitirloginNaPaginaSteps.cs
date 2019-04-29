4c362ef28822d7f7f9bbc3e88584d094bafcf297
Filipe FJ. Ferreira de Jesus <filipe.jesus@gx2.local>
2019-04-29 17:39:15 -0300

    carga projeto

---
+++ testeSpecFlow/code/PermitirloginNaPaginaSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace testeSpecFlow.code
+{
+    [Binding]
+    public class PermitirloginNaPaginaSteps
+    {
+        [Given(@"Dado que estou na pagina ""(.*)""")]
+        public void GivenDadoQueEstouNaPagina(string site)
+        {
+            
+        }
+        
+        [When(@"Eu informar ""(.*)"" no campo ""(.*)""")]
+        public void WhenEuInformarNoCampo(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Eu clicar no botao ""(.*)""")]
+        public void WhenEuClicarNoBotao(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Dever ser apresentado o text ""(.*)"" no campo ""(.*)""")]
+        public void ThenDeverSerApresentadoOTextNoCampo(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ testeSpecFlow/features/login.feature
@@ -0,0 +1,8 @@
+﻿Feature: Permitirlogin na pagina
+
+Scenario: Permitir login com sucesso
+	Given Dado que estou na pagina "https://automation-sandbox.herokuapp.com/"
+	When Eu informar "demouser" no campo "login"
+	And Eu informar "abc123" no campo "senha"
+	And Eu clicar no botao "btnlogar"
+	Then Dever ser apresentado o text "Invoice" no campo "label"
